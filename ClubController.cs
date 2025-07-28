07493e0d6f25dd32fa41e6a8a84fe11578db31f8
shahbano-eng <sha20210158@std.psut.edu.jo>
2025-07-28 13:31:02 +0300

    Add files via upload

---
+++ ClubController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Models;
+using RunGroupWebApp.Data;
+using System.Data.Entity;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _Context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _Context = context;
+        }
+        public IActionResult Index()  ///CCC
+        {
+            List<Club> clubs = _Context.Clubs.ToList(); ///MMM //brings whole table and applying querry to it by excecuting the sql
+            return View(clubs); ///VVV
+        }
+
+        public IActionResult Detail(int id)
+        {
+    
+            var clubList = _Context.Clubs.Include(a => a.Address).FirstOrDefault(c => c.Id == id);
+
+            if (clubList == null)
+            {
+                return NotFound(); 
+            }
+
+            return View(clubList); 
+        }
+    }
+}
---
+++ RaceController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Models;
+using RunGroupWebApp.Data;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Race> races = _context.Races.ToList();
+            return View(races);
+        }
+    }
+}
